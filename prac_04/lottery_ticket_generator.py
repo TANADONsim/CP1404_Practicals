439ac90e98accb2f8a06e8b658bc4fc5d7fa88a5
Tanadon Sim <hongweitanadonleonard.sim@my.jcu.edu.au>
2018-11-27 11:57:59 +0800

    Did Do-from-scratch exercise.

---
+++ prac_04/lottery_ticket_generator.py
@@ -0,0 +1,14 @@
+import random
+picks = int(input("How many quick picks?"))
+for pick in range(picks):
+    picks = []
+    picks.append(random.randrange(46))
+    for check in range(5):
+        check = random.randrange(46)
+        while check in picks:
+            check = random.randrange(46)
+        picks.append(random.randrange(46))
+    picks.sort()
+    print("{0:>4}{1:>4}{2:>4}{3:>4}{4:>4}{5:>4}".format(picks[0], picks[1], picks[2], picks[3], picks[4], picks[5]))
+
+
